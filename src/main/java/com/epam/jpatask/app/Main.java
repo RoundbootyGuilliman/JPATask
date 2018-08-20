873d6b891b92a3f31716e246a0d25556588b7f0c
Kristian_Koshaliyev <Kristian_Koshaliyev@epam.com>
2018-08-20 13:01:51 +0600

    [IMP] NOW IT WORKS

--- src/main/java/com/epam/jpatask/app/Main.java
+++ src/main/java/com/epam/jpatask/app/Main.java
@@ -23,39 +23,39 @@ public class Main {
 		ProjectDAO projectDAO = new ProjectDAO(entityManager);
 
 
-//		// create and persist new Unit
-//		Unit unit = new Unit("Unit");
-//		unitDAO.createUnit(unit);
-//
-//		// create and persist new Project
-//		Project project = new Project("Project");
-//		projectDAO.createProject(project);
-//
-//		// create and persist new Employee "Bob"
-//		Address address = new Address("US", "Boston", "23", "23", "55");
-//		Personal personal = new Personal("Bob", "Smith", new Date());
-//		Employee employee = new Employee("Cleaning manager", EmployeeStatus.ACTIVE, address, personal, unit,
-//				new ArrayList<Project>(){{add(project);}});
-//		employee.setEmployeeId(23);
-//		employeeDAO.createEmployee(employee);
-//
-//		// create and persist second Unit
-//		Unit unit2 = new Unit("Unit 2");
-//		unitDAO.createUnit(unit2);
-//
-//		// change Bob's Unit from Unit 1 to Unit 2
-//		employeeDAO.addToUnit(employee.getEmployeeId(), unit2.getUnitId());
-//
-//		// create and persist second Project
-//		Project project2 = new Project("Project 2");
-//		projectDAO.createProject(project2);
-//
-//		// add Project 2 to Bob's list of projects
-//		employeeDAO.assignToProject(employee.getEmployeeId(), project2.getProjectId());
+		// create and persist new Unit
+		Unit unit = new Unit("Unit");
+		unitDAO.createUnit(unit);
+
+		// create and persist new Project
+		Project project = new Project("Project");
+		projectDAO.createProject(project);
+
+		// create and persist new Employee "Bob"
+		Address address = new Address("US", "Boston", "23", "23", "55");
+		Personal personal = new Personal("Bob", "Smith", new Date());
+		Employee employee = new Employee("Cleaning manager", EmployeeStatus.ACTIVE, address, personal, unit,
+				new ArrayList<Project>(){{add(project);}});
+		employee.setEmployeeId(23);
+		employeeDAO.createEmployee(employee);
+
+		// create and persist second Unit
+		Unit unit2 = new Unit("Unit 2");
+		unitDAO.createUnit(unit2);
+
+		// change Bob's Unit from Unit 1 to Unit 2
+		employeeDAO.addToUnit(employee.getEmployeeId(), unit2.getUnitId());
+
+		// create and persist second Project
+		Project project2 = new Project("Project 2");
+		projectDAO.createProject(project2);
+
+		// add Project 2 to Bob's list of projects
+		employeeDAO.assignToProject(employee.getEmployeeId(), project2.getProjectId());
 		
-		employeeDAO.deleteAllEmployees();
-		unitDAO.deleteAllUnits();
-		projectDAO.deleteAllProjects();
+//		employeeDAO.deleteAllEmployees();
+//		unitDAO.deleteAllUnits();
+//		projectDAO.deleteAllProjects();
 		
 		entityManager.close();
 		factory.close();
